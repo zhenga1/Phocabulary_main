e91c7c12cd575d5af7ce81a456712781aba161cf
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-05-21 15:52:01 +0800

    add user avatar and username

--- Application/app/src/main/java/com/projectmonterey/MainActivity.java
+++ Application/app/src/main/java/com/projectmonterey/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
-    public void learn(View view){
+
+    public void learn(View view) {
         Intent intent = new Intent(this,UiMainActivity.class);
         startActivity(intent);
     }
