4cf57c1914ae881483baf6900342c0e0b7f3c52b
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-05-21 16:08:19 +0800

    settings page completed

--- Application/app/src/main/java/com/projectmonterey/Settings.java
+++ Application/app/src/main/java/com/projectmonterey/Settings.java
@@ -3,12 +3,18 @@ package com.projectmonterey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.List;
 
 public class Settings extends AppCompatActivity {
-
+    public List<LinearLayout> linearLayoutList;
+    public LinearLayout mainLL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        mainLL = findViewById(R.id.settingsLL);
+
     }
 }
