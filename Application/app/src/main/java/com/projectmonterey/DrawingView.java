8aaa82e048a840ffd33e9164d5c24fe7f877c0c3
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-05-21 16:36:35 +0800

    added cameraview

---
+++ Application/app/src/main/java/com/projectmonterey/DrawingView.java
@@ -0,0 +1,22 @@
+package com.projectmonterey;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawingView extends View {
+
+    public DrawingView(Context context) {
+        super(context);
+    }
+
+    public DrawingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
