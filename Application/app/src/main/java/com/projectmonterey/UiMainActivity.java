19e8b9834a2ffd6e638769e4ed3a85317bed6c19
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-05-21 14:52:19 +0800

    add activity_ui_main class

---
+++ Application/app/src/main/java/com/projectmonterey/UiMainActivity.java
@@ -0,0 +1,11 @@
+package com.projectmonterey;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UiMainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedBundleState) {
+        super.onCreate(savedBundleState);
+    }
+}
