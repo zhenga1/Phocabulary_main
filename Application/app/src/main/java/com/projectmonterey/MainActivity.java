605b2f28ffd5b96d34fd8ddf866431fc288ef4c8
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-05-22 20:21:47 +0800

    fix FirebaseApp.initializeApp again

--- Application/app/src/main/java/com/projectmonterey/MainActivity.java
+++ Application/app/src/main/java/com/projectmonterey/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        FirebaseApp.initializeApp(this);
 
         authInstance = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
