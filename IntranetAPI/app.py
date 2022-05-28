73f20855457b0a78a4c8f99e6e6bf6ab40ffa611
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-05-28 15:15:45 +0800

    add register endpoint

--- IntranetAPI/app.py
+++ IntranetAPI/app.py
@@ -1,8 +1,64 @@
-from flask import Flask
+import hashlib
+import os
+import random
+import time
+import string
 
+import asyncpg
+import dotenv
+from flask import Flask, request
+
+dotenv.load_dotenv()
 api = Flask(__name__)
 
 
-@api.route('/')
-def hello_world():
-    return "Hello World"
+@api.route('/test')
+async def hello_world():
+    conn = await asyncpg.connect(user=os.getenv("PGSQL_USERNAME"), password=os.getenv("PGSQL_PASSWORD"),
+                                 database="postgres", host="127.0.0.1")
+
+    async with conn.transaction():
+        cur = await conn.cursor('SELECT * FROM users')
+        result = await cur.fetchrow()
+
+        return {
+            'username': result['username']
+        }
+
+
+@api.route('/api/v1/auth/register', methods=['POST'])
+async def api_v1_auth_register():
+    if request.headers.get("Content-Type") != "application/json":
+        return {
+            'code': 400,
+            'status': 'Bad Request',
+            'message': 'expected JSON body'
+        }
+
+    conn = await asyncpg.connect(user=os.getenv("PGSQL_USERNAME"), password=os.getenv("PGSQL_PASSWORD"),
+                                 database="postgres", host="127.0.0.1")
+
+    try:
+        body = request.json
+
+        async with conn.transaction():
+            now = time.time()
+            salt = ''.join(random.choices(string.ascii_letters, k=10))
+
+            hashed_pwd = hashlib.sha256(bytes(body['password'] + salt, 'utf-8')).hexdigest()
+
+            await conn.execute(
+                f"INSERT TO users (username, pwd_saltedhash, user_id, salt) VALUES ('{body['username']}', '{hashed_pwd}', '{now}', '{salt}')"
+            )
+    except KeyError:
+        return {
+            'code': 422,
+            'status': 'Un-processable Entity',
+            'message': 'invalid request body'
+        }
+
+    return {
+        'code': 200,
+        'status': 'OK',
+        'message': 'the action completed successfully'
+    }
