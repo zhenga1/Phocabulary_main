449b1aa20aef02cf53b0d68468178c1e84a4582b
CareyLKS <leekingsang302@gmail.com>
2022-05-28 16:52:25 +0800

    fixed login bug

--- Application/app/src/main/java/com/projectmonterey/LoginActivity.java
+++ Application/app/src/main/java/com/projectmonterey/LoginActivity.java
@@ -10,23 +10,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText password,confpassword;
+    private EditText password,confpassword,uname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
         password = findViewById(R.id.editTextTextPassword2);
         confpassword=findViewById(R.id.editTextTextPassword3);
+        uname=findViewById(R.id.editTextTextPersonName2);
 
     }
     public void check(View view){
-        if(!(password.getText().toString().equals(confpassword.getText().toString()))){
-            Toast.makeText(getApplicationContext(),"Password and Confirmation Password do not match",Toast.LENGTH_SHORT).show();
+        if ((uname.getText().toString().isEmpty()) || (password.getText().toString().isEmpty())) {
+            Toast.makeText(getApplicationContext(),"Please fill in both username and password",Toast.LENGTH_SHORT).show();
         }
-        else{
+        else {
+            if(!(password.getText().toString().equals(confpassword.getText().toString()))){
+                Toast.makeText(getApplicationContext(),"Password and Confirmation Password do not match",Toast.LENGTH_SHORT).show();
+            }
+            else{
 
-            Intent intent = new Intent(this, MenuPage.class);
-            startActivity(intent);
+                Intent intent = new Intent(this, MenuPage.class);
+                startActivity(intent);
+            }
         }
+
     }
 }
