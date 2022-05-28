b254fac5083ca0479068cd7cd732dc7fba2a5bbb
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-05-28 14:07:24 +0800

    added Subviews for settings

---
+++ Application/app/src/main/java/com/projectmonterey/settings/set_General.java
@@ -0,0 +1,16 @@
+package com.projectmonterey.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.projectmonterey.R;
+
+public class set_General extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_general);
+    }
+}
