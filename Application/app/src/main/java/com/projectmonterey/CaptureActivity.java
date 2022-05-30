7d3bb832792fda27a562af798f10f465331b1f0c
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-05-30 11:50:33 +0800

    fixed capture problem, captured image no longer transposed

--- Application/app/src/main/java/com/projectmonterey/CaptureActivity.java
+++ Application/app/src/main/java/com/projectmonterey/CaptureActivity.java
@@ -3,6 +3,7 @@ package com.projectmonterey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +15,13 @@ public class CaptureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
         View view = findViewById(R.id.capturecanvasview);
-        BitmapDrawable bitmapDrawable = new BitmapDrawable(CaptureActivity.this.getResources(),CameraActivity.captureimg);
+        BitmapDrawable bitmapDrawable = new BitmapDrawable(CaptureActivity.this.getResources(),transposeBitmap(CameraActivity.captureimg));
         view.setBackground(bitmapDrawable);
+    }
+    public Bitmap transposeBitmap(Bitmap b){
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90);
+        Bitmap newbitmap = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(), matrix, true);
+        return newbitmap;
     }
 }
