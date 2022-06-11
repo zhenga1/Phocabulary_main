18d6f0591b8fe8e8e00f6e6184da28be1b8c3583
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-06-11 14:45:40 +0800

    saved capture file

--- Application/app/src/main/java/com/projectmonterey/CaptureActivity.java
+++ Application/app/src/main/java/com/projectmonterey/CaptureActivity.java
@@ -3,7 +3,11 @@ package com.projectmonterey;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -12,12 +16,9 @@ import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
-import android.hardware.Camera;
-import android.hardware.Camera.FaceDetectionListener;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.os.Environment;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -25,23 +26,30 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.face.Face;
-import com.google.mlkit.vision.face.FaceContour;
 import com.google.mlkit.vision.face.FaceDetection;
 import com.google.mlkit.vision.face.FaceDetector;
 import com.google.mlkit.vision.face.FaceDetectorOptions;
 import com.google.mlkit.vision.face.FaceLandmark;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CaptureActivity extends AppCompatActivity {
     private FaceDetectorOptions highAccuracyOpts,realTimeOpts;
     private List<Rect> boundingboxes;
+    private int scrnnumb = 0;
     private FaceOverlayView faceOverlayView;
+    private Bitmap bitmap;
     private ConstraintLayout parent;
     private List<Face> thefaces;
-
     private float rotY,rotZ,smileProb,rightEyeOpenProb;
+    private int WRITE_REQUEST_CODE=10;
+    private int READ_REQUEST_CODE=11;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,9 +57,13 @@ public class CaptureActivity extends AppCompatActivity {
         parent = findViewById(R.id.parent_constraint_layout);
         addFaceLayout();
         CaptureView view = (CaptureView) findViewById(R.id.capturecanvasview);
-        Bitmap bitmap = transposeBitmap(CameraActivity.captureimg);
+        bitmap = transposeBitmap(CameraActivity.captureimg);
         BitmapDrawable bitmapDrawable = new BitmapDrawable(CaptureActivity.this.getResources(),bitmap);
         view.setBackground(bitmapDrawable);
+        if(savePermissions()) {
+            saveCaptureToFile();
+        }
+
         // High-accuracy landmark detection and face classification
         highAccuracyOpts = new FaceDetectorOptions.Builder()
                         .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
@@ -109,6 +121,65 @@ public class CaptureActivity extends AppCompatActivity {
                                 });
 
     }
+    private boolean savePermissions(){
+        int write = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int read = ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(write == PackageManager.PERMISSION_GRANTED && read ==PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        if(write != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                {
+                    Toast.makeText(getApplicationContext(),"The application needs the permission of write storage in order to save the drawn image",Toast.LENGTH_LONG).show();
+                }
+                ActivityCompat.requestPermissions(CaptureActivity.this,
+                        new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                        WRITE_REQUEST_CODE);
+            }
+        }
+        if(read != PackageManager.PERMISSION_GRANTED)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE))
+                {
+                    Toast.makeText(getApplicationContext(),"The application needs the permission of write storage in order to save the drawn image",Toast.LENGTH_LONG).show();
+                }
+                ActivityCompat.requestPermissions(CaptureActivity.this,
+                        new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                        READ_REQUEST_CODE);
+            }
+        }
+        return false;
+    }
+    private File getreqdcapfile(){
+        File file = new File(Environment.getExternalStorageDirectory()+"/Captures/"+"scrnfile"+Integer.toString(scrnnumb)+".jpg");
+        while (file.exists()){
+            scrnnumb +=1;
+            file = new File(Environment.getExternalStorageDirectory()+"/Captures/"+"scrnfile"+Integer.toString(scrnnumb)+".jpg");
+        }
+        return file;
+    }
+    private void saveCaptureToFile(){
+        File folder = new File(Environment.getExternalStorageDirectory()+"/Captures");
+        boolean success = true;
+        if(!folder.exists()) success = folder.mkdir();
+        if(success){
+            File file = getreqdcapfile();
+            scrnnumb+=1;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        bitmap.compress(Bitmap.CompressFormat.JPEG,100,new FileOutputStream(file));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            Toast.makeText(getApplicationContext(),"Sucessfully captured and saved image",Toast.LENGTH_SHORT).show();
+        }
+    }
     private void addFaceLayout(){
         faceOverlayView = new FaceOverlayView(this);
         faceOverlayView.setLayoutParams(new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.FILL_PARENT, ConstraintLayout.LayoutParams.FILL_PARENT));
@@ -150,6 +221,15 @@ public class CaptureActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==WRITE_REQUEST_CODE){
+            saveCaptureToFile();
+        }
+    }
+
     public Bitmap transposeBitmap(Bitmap b){
         //Rotate bitmap via empty matrix
         Matrix matrix = new Matrix();
