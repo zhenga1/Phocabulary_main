ac01636ad76d8247a127072b01fd94d967bf873a
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-06-19 18:47:46 +0800

    reorganise

---
+++ Application/app/src/main/java/com/projectmonterey/yolo/CaptureActivityYolo.java
@@ -0,0 +1,14 @@
+package com.projectmonterey.yolo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.projectmonterey.R;
+import android.os.Bundle;
+
+public class CaptureActivityYolo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_yolo);
+    }
+}
