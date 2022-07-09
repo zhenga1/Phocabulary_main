68d2f4d76a7aa5c73afc6d39042496435c9a8df8
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-07-09 14:38:03 +0800

    fixed welcome screen

--- Application/app/src/main/java/com/projectmonterey/ui_main/WelcomeScreen.java
+++ Application/app/src/main/java/com/projectmonterey/ui_main/WelcomeScreen.java
@@ -10,22 +10,22 @@ import android.os.Handler;
 import com.projectmonterey.MainActivity;
 import com.projectmonterey.R;
 
-public class Welcome_SCreen extends AppCompatActivity {
+public class WelcomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
         //getSupportActionBar().hide();
-        Intent i = new Intent(Welcome_SCreen.this, MainActivity.class);
+        Intent i = new Intent(WelcomeScreen.this, MainActivity.class);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Welcome_SCreen.this, MainActivity.class);
-                Welcome_SCreen.this.startActivity(i);
+                Intent i = new Intent(WelcomeScreen.this, MainActivity.class);
+                WelcomeScreen.this.startActivity(i);
                 startActivity(i);
                 // close this activity
-                Welcome_SCreen.this.finish();
+                WelcomeScreen.this.finish();
             }
         }, 1500);
     }
