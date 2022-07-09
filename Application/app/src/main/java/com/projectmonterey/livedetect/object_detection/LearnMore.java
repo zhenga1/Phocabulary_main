2470d5754e37e43ae44bd3ad8653d1862ac74fec
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-07-09 16:40:20 +0800

    fixed learnmore page image

--- Application/app/src/main/java/com/projectmonterey/livedetect/object_detection/LearnMore.java
+++ Application/app/src/main/java/com/projectmonterey/livedetect/object_detection/LearnMore.java
@@ -28,15 +28,16 @@ public class LearnMore extends AppCompatActivity {
         Intent intent=getIntent();
         String name=intent.getStringExtra("Name");
         String deff=intent.getStringExtra("DEF");
-        String lk;
-        //lk=intent.getStringExtra("PHO");
-        lk="https://imgur.com/hNXP3t2";
+        //String lk;
+        //lk=
+        //lk="https://imgur.com/hNXP3t2";
+        String link = "https://media.istockphoto.com/photos/grey-reusable-bottle-on-grey-background-picture-id1299291084?b=1&k=20&m=1299291084&s=612x612&w=0&h=hGhYT35eg9QMS7PymtPdWN9_y9GCeks5Nr7MUjFj3D0=";
         obj=findViewById(R.id.OBJ);
         obj.setText(name);
         def=findViewById(R.id.DEF);
         def.setText(deff);
         pic = findViewById(R.id.WEB);
-        pic.loadUrl(lk);
+        pic.loadUrl(link);
         //setContentView(pic);
     }
     public void go_back(View view){
