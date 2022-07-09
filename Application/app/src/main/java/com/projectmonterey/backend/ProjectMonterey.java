5259abe6bf655347a888d3e1aeee2760e11c39ad
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-07-09 14:34:08 +0800

    fixed landscape orientation

--- Application/app/src/main/java/com/projectmonterey/backend/ProjectMonterey.java
+++ Application/app/src/main/java/com/projectmonterey/backend/ProjectMonterey.java
@@ -1,6 +1,10 @@
 package com.projectmonterey.backend;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import com.google.firebase.FirebaseApp;
 
 public class ProjectMonterey extends Application {
@@ -9,5 +13,52 @@ public class ProjectMonterey extends Application {
         super.onCreate();
 
         FirebaseApp.initializeApp(this);
+        initialiseActivityCallback();
     }
+
+    private void initialiseActivityCallback() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                activity.setRequestedOrientation(
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+// for each activity this function is called and so it is set to portrait mode
+
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+
 }
