6f7834b147ed81ae218d84f5617bd17d2032c2d2
careyLKS <leekingsang302@gmail.com>
2022-07-21 21:18:52 +0800

    UI :)

--- Application/app/src/main/java/com/projectmonterey/ui_main/MenuPage.java
+++ Application/app/src/main/java/com/projectmonterey/ui_main/MenuPage.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import com.projectmonterey.R;
 import com.projectmonterey.capturedetect.CameraActivity;
+import com.projectmonterey.livedetect.object_detection.CameraActivityYolo;
 
 public class MenuPage extends AppCompatActivity {
 
@@ -21,7 +22,7 @@ public class MenuPage extends AppCompatActivity {
         startActivity(intent);
     }
     public void learn(View view) {
-        Intent intent = new Intent(this, CameraActivity.class);
+        Intent intent = new Intent(this, CameraActivityYolo.class);
         startActivity(intent);
     }
     public void setting(View view) {
