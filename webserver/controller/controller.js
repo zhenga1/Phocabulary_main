2ea9a99b13659ee3c2cba8e3e88643119b1466f6
Aaron Zheng <aaronhaowenzheng@gmail.com>
2022-07-21 20:11:30 +0800

    fixed web stuff

--- webserver/controller/controller.js
+++ webserver/controller/controller.js
@@ -2,21 +2,21 @@ const page_main = (req,res)=>{
     res.render('index',{title:"Phocabulary", slogan:"Learn with ease!"})
 }
 const page_about = (req,res)=>{
-
+    res.render('about');
 }
 const page_login = (req,res)=>{
     console.log("successful call");
     res.render('login');
 }
 const page_register = (req,res)=>{
-
+    console.log('calling register function');
+    res.render('register')
 }
 const page_share = (req,res)=>{
-
+    res.render('share');
 }
 const login_page_post = (req,res)=>{
-
-    
+        
 }
 module.exports={
     page_main,
